6c2c30caa464dc54d4f71c61ee93061fca4d627a
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-06-10 01:49:13 -0400

    Add mustache.js for message templates

--- server/server.js
+++ server/server.js
@@ -12,33 +12,22 @@ let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// LISTEN FOR CLIENT CONNECTION
 io.on('connection', (socket) => {
   console.log('Client Connected');
-
-  // ADMIN: WELCOME MESSAGE
   socket.emit('newMessage', generateMessage('Admin', 'Welcome!'));
-
-  // ADMIN: NEW USER MESSAGE
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-
-  // LISTEN FOR 'createMessage' EVENT
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
-
-    // EMIT RECIEVED MESSAGE
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback();
   });
 
-  // SEND 'newLocationMessage' TO DOM
   socket.on('createLocatonMessage', (coords) => {
     io.emit('newLocationMessage', generateLocationMessage(
       'Admin', coords.latitude, coords.longitude
     ));
   });
 
-  // LISTEN FOR CLIENT DISCONNECT
   socket.on('disconnect', () => {
     console.log('Client Disconnected');
   });
