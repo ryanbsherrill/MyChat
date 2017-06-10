ee477cf6f9c93545bc5a2e04390453806dc26bbe
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-06-10 02:24:39 -0400

    Scroll to bottom if user is close

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 const socket = io();
 
+function scrollToBottom () {
+  let messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child');
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (
+    clientHeight +
+    scrollTop +
+    newMessageHeight +
+    lastMessageHeight >=
+    scrollHeight) {
+      messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Server Connected');
 });
@@ -18,6 +37,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -30,6 +50,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
